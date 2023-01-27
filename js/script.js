ca36efaa4ce42326810f17ef4dae2fff847b10a8
laura_z <llzerr@hotmail.com>
2023-01-27 15:21:46 -0700

    Update to fetch and display repo data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // profile information div
 const profileOverview = document.querySelector(".overview");
 const username = "ll-zerr";
+const listOfRepos = document.querySelector(".repo-list");
 
 // fetch data from user profile
 const getData = async function() {
@@ -29,4 +30,23 @@ const displayData = function (data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
    </div>`
    profileOverview.append(profileInfo);
+   getRepos();
+};
+
+// fetch user repos
+const getRepos = async function () {
+  const repoResult = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await repoResult.json();
+    console.log(repoData);
+    repoInfo(repoData);
+};
+
+// display info about repos
+const repoInfo = function (repos) {
+  for (let repo of repos) {
+    let listItem = document.createElement("li");
+    listItem.classList.add("repo");
+    listItem.innerHTML = `<h3>${repo.name}</h3>`;
+    listOfRepos.append(listItem);
+  }
 };
